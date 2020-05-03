58cca9d5f57f0c6dab4b075ea7643347855fac62
Lena <kotenevaelena13@g.mail.com>
2020-05-03 03:53:50 +0300

    add dynamic blocks

--- js/mainjs.js
+++ js/mainjs.js
@@ -24,13 +24,7 @@ let processes = [
 	{id: 4, title: 'Ko Image Lara', preContent: 'Lorem ipsum is dummy ry dumy Lorem ipsum is.', content: 'Lorem ipsum is dummy lorem very dumy. Lorem ipsum is dummy ry dumy Lorem ipsum is.', icon: 'fa-camera-retro', colorNum: 'fourth'},
 
 ]
-let design = {id: 'design', title: 'Modern App Design', content: `
-<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus earum totam ad sint error porro. Consectetur, sunt, aperiam. Magni rem optio distinctio illum consequuntur delectus nulla illo aperiam itaque cumque.</p>
-<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel officia vitae eveniet quam rem unde facilis.</p>
-<p>vitae eveniet quam rem unde facilis.</p>
-`}
-
-const toHTML = process => `
+const toHTMLprocess = process => `
 <div class="job col-1 ${process.colorNum}">
 <h2><i class="fa ${process.icon}"></i></h2>
 <h3>${process.title}</h3>
@@ -38,9 +32,73 @@ const toHTML = process => `
 <button class="more" data-btn='more' data-id='${process.id}'>+ Know More</button>
 </div>`
 
+let design = {id: 'design', title: 'Modern App Design', content: `
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus earum totam ad sint error porro. Consectetur, sunt, aperiam. Magni rem optio distinctio illum consequuntur delectus nulla illo aperiam itaque cumque.</p>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel officia vitae eveniet quam rem unde facilis.</p>
+<p>vitae eveniet quam rem unde facilis.</p>
+`}
+
+let works = [
+{id: 1}, {id: 2}, {id: 3}, {id: 4}, {id: 5}, {id: 6}, {id: 7},{id: 8} ,{id: 9}
+]
+
+const toProjects = project => `
+<div class="project col-1" id="${project.id}"></div>
+`
+
+let team = [
+{id: '1', name: 'Bendi Manos', position: 'UI/UX Parena'},
+{id: '2', name: 'Al Rayhan', position: 'UI/UX Designer'},
+{id: '3', name: 'Ali Sayed', position: 'UI/UX Designer'}
+]
+
+const toPersons = person => `
+ <div class="worker col-1">
+<div class="who">
+<h3>${person.name}</h3>
+<p>${person.position}</p>
+</div>
+</div>
+`
+
+let carouselItems = [
+{slideContent: `<p>“This was my first time renting from car rental. The service rep help me find the best rental to suit my needs. I will definitely rent with this company again”</p><p>Kita Say, HK Director</p>`},
+{slideContent: `<p>“This was my first time renting from car rental. The service rep help me find the best rental to suit my needs. I will definitely rent with this company again”</p><p>Dita Say, UX Director</p>`},
+{slideContent: `<p>“This was my first time renting from car rental. The service rep help me find the best rental to suit my needs. I will definitely rent with this company again”</p><p>Nita Say, HK Director</p>`},
+{slideContent: `<p>“This was my first time renting from car rental. The service rep help me find the best rental to suit my needs. I will definitely rent with this company again”</p><p>Anita Say, HK Director</p>`},
+{slideContent: `<p>“This was my first time renting from car rental. The service rep help me find the best rental to suit my needs. I will definitely rent with this company again”</p><p>Al Rayhan, UX Director</p>`},
+{slideContent: `<p>“This was my first time renting from car rental. The service rep help me find the best rental to suit my needs. I will definitely rent with this company again”</p><p>Alina Rayhan, UX Director</p>`}
+]
+const toCarousel = carousel => `
+ <div class="carousel-item"><div class="aligner">${carousel.slideContent}</div></div>
+`
+
+let social = [
+{href: '#', icon: 'fa-facebook-f'},
+{href: '#', icon: 'fa-twitter'},
+{href: '#', icon: 'fa-vk'},
+{href: '#', icon: 'fa-instagram'}
+]
+
+const toSocial = net => `
+<a href="${net.href}" target="_blank"><i class="fa ${net.icon}"></i></a>
+`
+
 function render() {
-	const html = processes.map(process => toHTML(process)).join('')
-	document.querySelector('.processes').innerHTML = html
+	const htmlProc = processes.map(process => toHTMLprocess(process)).join('')
+	document.querySelector('.processes').innerHTML = htmlProc
+
+	const htmlCarousel = carouselItems.map(carousel => toCarousel(carousel)).join('')
+	document.querySelector('.my-carousel').innerHTML = htmlCarousel
+	
+	const htmlWorks = works.map(project => toProjects(project)).join('')
+	document.querySelector('.projects').innerHTML = htmlWorks
+	
+	const htmlTeam = team.map(person => toPersons(person)).join('')
+	document.querySelector('.our-team').innerHTML = htmlTeam
+	
+	const htmlNet = social.map(net => toSocial(net)).join('')
+	document.querySelector('.social .col-1').innerHTML = htmlNet
 }
 render()
 
@@ -71,12 +129,7 @@ const modal = $.modal({})
     };
 
 
-
-
-
-
 /*
-
 
 //contact-form
  $(document).ready(function() {
